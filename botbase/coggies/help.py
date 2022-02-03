e46a596bda4284c0bae35c1ab875b409a52ea94b
ooliver1 <oliverwilkes2006@icloud.com>
2022-02-03 21:47:59 +0000

    if in doubt, log with the wrong level

--- botbase/coggies/help.py
+++ botbase/coggies/help.py
@@ -4,8 +4,9 @@ from difflib import get_close_matches
 from inspect import cleandoc
 from itertools import groupby
 from sys import stderr
-from traceback import print_exception
+from traceback import log.info_exception
 from typing import TYPE_CHECKING
+from logging import getLogger
 
 from nextcord import ButtonStyle, Embed
 from nextcord.ext.commands import Cog, HelpCommand, command
@@ -20,6 +21,9 @@ if TYPE_CHECKING:
     from nextcord.ui import Item
 
     from ..botbase import BotBase
+
+
+log = getLogger(__name__)
 
 
 class MyMenu(ButtonMenuPages):
@@ -77,6 +81,7 @@ class FrontPageSource(PageSource):
         return self
 
     def format_page(self, menu: MyMenu, _) -> Embed:
+        log.info("1")
         assert menu.ctx.bot.user is not None
         embed = Embed(
             title=menu.ctx.bot.helptitle,
@@ -85,6 +90,8 @@ class FrontPageSource(PageSource):
             ),
             color=menu.ctx.bot.color,
         )
+
+        log.info("2")
 
         for name, value in menu.ctx.bot.helpfields.items():
             embed.add_field(
@@ -119,6 +126,8 @@ class FrontPageSource(PageSource):
             for name, value in entries:
                 embed.add_field(name=name, value=value, inline=False)
 
+        log.info("3")
+
         return embed
 
 
@@ -132,6 +141,7 @@ class HelpSelect(Select):
         self.commands = commands
         self.bot = bot
         self.fill()
+        log.info("z")
 
     def fill(self) -> None:
         self.add_option(
@@ -151,6 +161,7 @@ class HelpSelect(Select):
                 value=cog.qualified_name,
                 description=description,
             )
+        log.info("y")
 
     async def callback(self, interaction: Interaction):
         assert self.view is not None
@@ -205,7 +216,7 @@ class HelpView(ButtonMenuPages):
             await interaction.response.send_message(
                 "An unknown error occurred, sorry", ephemeral=True
             )
-        print_exception(type(error), error, error.__traceback__, file=stderr)
+        log.info_exception(type(error), error, error.__traceback__, file=stderr)
 
     async def interaction_check(self, interaction: Interaction) -> bool:
         if interaction.user and interaction.user.id in (
@@ -250,6 +261,7 @@ class MyHelp(HelpCommand):
         return f"{self.context.clean_prefix}{name} {command.signature}"
 
     async def send_bot_help(self, mapping: dict[Cog, Command]) -> None:
+        log.info("a")
         bot = self.context.bot
 
         if coggie := bot.get_cog("Help"):
@@ -258,6 +270,8 @@ class MyHelp(HelpCommand):
         def key(command) -> str:
             cog = command.cog
             return cog.qualified_name if cog else "\U0010ffff"
+
+        log.info("b")
 
         entries: list[Command] = [
             c for c in sorted(bot.commands, key=key) if not c.hidden
@@ -272,8 +286,14 @@ class MyHelp(HelpCommand):
             if cog is not None:
                 all_commands[cog] = sorted(children, key=lambda c: c.qualified_name)
 
+        log.info("c")
+
         menu = HelpView(FrontPageSource(), ctx=self.context, cmds=all_commands)  # type: ignore
-        await menu.start(self.context)
+        log.info("d")
+        try:
+            await menu.start(self.context)
+        except Exception as e:
+            log.info(e)
 
     async def send_cog_help(self, cog: Cog):
         entries: list[Command] = [
