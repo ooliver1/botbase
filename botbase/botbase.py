a0d012b33a7b881c2d306a2a1cfd0fcfb0ab229e
ooliver1 <oliverwilkes2006@icloud.com>
2022-02-03 17:50:46 +0000

    oh so thats what that means

--- botbase/botbase.py
+++ botbase/botbase.py
@@ -63,7 +63,7 @@ class BotBase(Bot):
         )
         h.setFormatter(
             Formatter(
-                "%(levelname)s %(asctime)s %(filename)12s:%(funcName)-12s: %(message)s",
+                "%(levelname)-7s %(asctime)s %(filename)12s:%(funcName)-13s: %(message)s",
                 datefmt="%H:%M:%S %d/%m/%Y",
             )
         )
