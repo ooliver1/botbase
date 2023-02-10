c07ff545458ea76e0f2c928dab421f778f8ccbf0
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-02-10 12:50:28 +0000

    refactor!: remove emojis dict shim

--- botbase/__init__.py
+++ botbase/__init__.py
@@ -2,7 +2,6 @@ from logging import NullHandler, getLogger
 
 from .botbase import *
 from .checks import *
-from .emojis import *
 from .exceptions import *
 from .wraps import *
 from .models import *
