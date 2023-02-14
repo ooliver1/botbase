1abb10ef584c59a162a7103638ccc028f4fc049e
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-02-14 14:44:42 +0000

    fix: attempt commandlog composte patch

--- botbase/db/commands.py
+++ botbase/db/commands.py
@@ -12,9 +12,15 @@ class CommandLog(Model):
         constraints = [UniqueConstraint("command", "guild", "channel", "member")]
 
     # pyright: reportGeneralTypeIssues=false
-    id: int = Integer(primary_key=True, autoincrement=True)
-    command: str = String(max_length=255)
+    command: str = String(max_length=255, primary_key=True)
     guild: int = BigInteger()
     channel: int = BigInteger()
     member: int = BigInteger()
     amount: int = BigInteger(default=1)
+
+
+# ormar :(
+CommandLog.command.primary_key = True
+CommandLog.guild.primary_key = True
+CommandLog.channel.primary_key = True
+CommandLog.member.primary_key = True
